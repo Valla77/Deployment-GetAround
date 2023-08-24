736f4afcc1d0fd21de5863c7807efc0f289925d9
Valerie MUTHIANI <vievalla7@gmail.com>
2023-08-24 13:21:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,10 @@ st.markdown("---")
 
 st.markdown("""
     The concept is straightforward, yet regrettably, there are instances where drivers arrive late for checkouts. Such scenarios can pose challenges, particularly if there's a subsequent rental scheduled immediately after. Presented below are data-driven insights to empower you in making informed choices regarding the optimal interval between successive car rentals.  👇
+    "- The **threshold** (the minimum time gap required between two rentals) \n" \
+    "- The **scope** of the threshold's application (whether it should be applied to all cars, mobile or only to 'Connect'* cars)."
 """)
+st.caption("_\* 'Connect cars': the driver doesn’t meet the owner and opens the car with his smartphone_   _\* '📱 Mobile': rental agreement on native apps: driver and owner meet and both sign the rental agreement on the owner’s smartphone")
 
 # Set title and markdown 
 st.title("Getaround Analysis on Delays ⏱")
